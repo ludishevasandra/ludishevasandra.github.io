c01981bc8c2bd8c8794dfe62a2b7cf51599933b4
alexandra <main-place@outlook.com>
2025-07-29 17:29:27 +0700

    creating robots.txt and sitemap.xml; domstroy - fixing categories and pagination

--- domstroy/wp-content/plugins/blocks-for-domstroy/build/dialog-button/view.js
+++ domstroy/wp-content/plugins/blocks-for-domstroy/build/dialog-button/view.js
@@ -1,17 +1,1 @@
-/******/ (() => { // webpackBootstrap
-/*!***********************************!*\
-  !*** ./src/dialog-button/view.js ***!
-  \***********************************/
-function openModal() {
-  const dialog = this.closest(".wp-block-create-block-dialog-button").lastElementChild;
-  if (dialog.matches(".wp-block-create-block-dialog")) {
-    dialog.showModal();
-    document.body.classList.add("scroll-lock");
-  }
-}
-if (document.querySelector(".wp-block-create-block-dialog-button .wp-block-button button")) {
-  document.querySelectorAll(".wp-block-create-block-dialog-button .wp-block-button button").forEach(button => button.onclick = openModal);
-}
-/******/ })()
-;
-//# sourceMappingURL=view.js.map
+(()=>{function o(){const o=this.closest(".wp-block-create-block-dialog-button").lastElementChild;o.matches(".wp-block-create-block-dialog")&&(o.showModal(),document.body.classList.add("scroll-lock"))}document.querySelector(".wp-block-create-block-dialog-button .wp-block-button button")&&document.querySelectorAll(".wp-block-create-block-dialog-button .wp-block-button button").forEach(t=>t.onclick=o)})();
--- domstroy/wp-content/plugins/blocks-for-domstroy/build/go-to-top/view.js
+++ domstroy/wp-content/plugins/blocks-for-domstroy/build/go-to-top/view.js
@@ -1,14 +1,1 @@
-/******/ (() => { // webpackBootstrap
-/*!*******************************!*\
-  !*** ./src/go-to-top/view.js ***!
-  \*******************************/
-function scrollToTop() {
-  document.documentElement.scrollIntoView({
-    behavior: 'smooth',
-    block: 'start'
-  });
-}
-document.querySelector(".wp-block-create-block-go-to-top .go-to-top__button").onclick = scrollToTop;
-/******/ })()
-;
-//# sourceMappingURL=view.js.map
+document.querySelector(".wp-block-create-block-go-to-top .go-to-top__button").onclick=function(){document.documentElement.scrollIntoView({behavior:"smooth",block:"start"})};
